98ff5edb921b1dd840bf68811ae07d595d1ccd27
Monisha-Elumalai <126869857+Monisha-Elumalai@users.noreply.github.com>
2025-12-16 19:36:46 +0530

    commit 2

--- Java_Code/src/com/tnsif/basics/MyFirstProgram.java
+++ Java_Code/src/com/tnsif/basics/MyFirstProgram.java
@@ -2,7 +2,7 @@ package com.tnsif.basics;
 
 public class MyFirstProgram {
 	public static void main(String[] args) { 
-		System.out.println("Hello World");
+		System.out.println("Welcome to github");
 
 	}
 
