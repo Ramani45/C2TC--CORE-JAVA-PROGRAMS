6e4222b00e509f01bd14915dae02c4f8644bc7c9
Monisha-Elumalai <126869857+Monisha-Elumalai@users.noreply.github.com>
2025-12-16 19:23:15 +0530

    commit 1

---
+++ Java_Code/src/com/tnsif/basics/ForLoopExample.java
@@ -0,0 +1,14 @@
+package com.tnsif.basics;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=1000;i++) {
+			System.out.println("value of i");
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
